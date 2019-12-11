ded810737cd587e95ae0dfb402c33adcc7391809
amr-fouda <eng.amr.mohamed.saleh@gmail.com>
2019-12-11 12:08:40 +0100

    Adding min loss challenge

--- README.md
+++ README.md
@@ -7,7 +7,8 @@ Practicing algorithms using jdk 8 maven project
 
 ## Level : Easy
 
-| N| Name | Level |  Category |
-| :-------------: |:-------------|:-------------|:-------------|
-| 1 |CatAndMouse.java|Easy|Algorithms|
-| 2 |Pangrams.java|Easy|String|
+| N| Name | Level |  Category |Link|
+| :-------------: |:-------------|:-------------|:-------------|-------|
+| 1 |CatAndMouse.java|Easy|Algorithms|https://www.hackerrank.com/challenges/cats-and-a-mouse/problem|
+| 2 |Pangrams.java|Easy|String|https://www.hackerrank.com/challenges/pangrams/problem|
+| 3 |MinimumLoss.java|Medium|Search|https://www.hackerrank.com/challenges/minimum-loss/problem|
---
+++ src/main/java/org/fouda/solutions/implementation/MinimumLoss.java
@@ -0,0 +1,28 @@
+package org.fouda.solutions.implementation;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+class MinimumLoss {
+    // Complete the minimumLoss function below.
+    static int minimumLoss(long[] price) {
+        long min = Long.MAX_VALUE;
+        for (int i = 0; i < price.length; i++) {
+            for (int y = i + 1; y < price.length; y++) {
+                long loss = price[i] - price[y];
+                if (loss > 0 && min > loss) {
+                    min = loss;
+                }
+            }
+
+        }
+        return (int) min;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        assert 2 == minimumLoss(new long[]{20, 7, 8, 2, 5});
+        assert 2 == minimumLoss(new long[]{5, 10, 3});
+    }
+}
