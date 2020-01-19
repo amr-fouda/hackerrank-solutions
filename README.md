096dc2892b1f37dfb0757b2b94dac9712658099d
amr-fouda <eng.amr.mohamed.saleh@gmail.com>
2020-01-19 19:35:09 +0100

    adding time conversion problem

--- README.md
+++ README.md
@@ -20,3 +20,5 @@ Practicing algorithms using jdk 8 maven project
 | 9 |[IcecreamParlor.java](src/main/java/org/fouda/solutions/IcecreamParlor.java)|Easy|Search|https://www.hackerrank.com/challenges/icecream-parlor/problem|
 |10 |[MissingNumbers.java](src/main/java/org/fouda/solutions/MissingNumbers.java)|Easy|Search|https://www.hackerrank.com/challenges/missing-numbers/problem|
 |11 |[ElectronicsShop.java](src/main/java/org/fouda/solutions/ElectronicsShop.java)|Easy|Implementation|https://www.hackerrank.com/challenges/electronics-shop/problem|
+|12 |[TimeConversion.java](src/main/java/org/fouda/solutions/TimeConversion.java)|Easy|Warm up|https://www.hackerrank.com/challenges/time-conversion/problem|
+
---
+++ src/main/java/org/fouda/solutions/TimeConversion.java
@@ -0,0 +1,28 @@
+package org.fouda.solutions;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class TimeConversion {
+    /*
+     * Complete the timeConversion function below.
+     */
+    static String timeConversion(String s) throws Exception {
+        return LocalTime.parse(s, DateTimeFormatter.ofPattern("hh:mm:ssa")).format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(timeConversion("07:05:45PM").equalsIgnoreCase("19:05:45"));
+        System.out.println(timeConversion("12:00:00AM").equalsIgnoreCase("00:00:00"));
+        System.out.println((timeConversion("12:00:00AM")));
+        /*
+            Symbol  Meaning                     Presentation      Examples
+            ------  ------------                ------------      --------
+            a       am-pm-of-day                text              PM
+            h       clock-hour-of-am-pm (1-12)  number            12
+            K       hour-of-am-pm (0-11)        number            0
+            k       clock-hour-of-am-pm (1-24)  number            0
+            H       hour-of-day (0-23)          number            0
+        */
+    }
+}
